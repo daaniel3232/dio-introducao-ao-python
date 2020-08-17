327063f0c16df86fc033327c5edb66cae5d5eb3b
daniel <daaniel3232@gmail.com>
2020-08-17 13:20:20 -0300

    aula06 - conjunto e subconjunto

---
+++ README.md
@@ -0,0 +1,1 @@
+dio introducao ao python
---
+++ aula05.py
@@ -0,0 +1,34 @@
+lista = [1, 3, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+
+
+# print(lista_animal[1])
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# lista.reverse()
+# lista_animal.reverse()
+# print(lista)
+# print(lista_animal)
+# soma = 0
+# for x in lista:
+#     print(x)
+#     soma += x # Soma todos os números da lista 7 + 5 + 3 + 1
+# print(soma)
+
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('não existe um lobo na lista. Será incluído.')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+# lista_animal.pop(1)
+# print(lista_animal)
+
+# lista_animal.remove('elefante')
+# print(lista_animal)
